f6239232b97fd329a98ba1be9f7c88ebbeee56eb
xiaobing1103 <472427503@qq.com>
2024-04-02 18:28:33 +0800

    tiajio

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <el-row>
+      <el-button @click="drawer = true" type="primary" style="margin-left: 16px;">
+        点我打开
+      </el-button>
+    </el-row>
+
+    <AppDrawer :drawer="drawer" :direction="direction" @handleClose="handleClose" />
+  </div>
+</template>
+
+<script>
+import AppDrawer from './components/AppDrawer.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      drawer: false,
+      direction: 'rtl',
+    };
+  },
+  components: {
+    // HelloWorld
+    AppDrawer
+  },
+  methods: {
+    handleClose(done) {
+      this.$confirm('确认关闭？')
+        .then(_ => {
+          console.log(_)
+          this.drawer = !this.drawer
+          done();
+        })
+
+        .catch(_ => {
+          console.log(_)
+        });
+    }
+  }
+}
+</script>
+
